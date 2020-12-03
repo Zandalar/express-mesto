c29131c08411552fac9072ddf9b8d656b57edea1
Zandalar <vs.masich@gmail.com>
2020-12-03 11:11:40 +0300

    feat: server complete

---
+++ routes/users.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const path = require('path');
+const fileReader = require('./fileReader');
+
+const filepath = path.join(__dirname, '..', 'data', 'users.json');
+
+router.get('/users', (req, res) => {
+  fileReader(filepath)
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(500).send(err))
+})
+
+router.get('/users/:id', (req, res) => {
+  fileReader(filepath)
+    .then((data) => {
+      let status = 404;
+      let answer = { message: 'Нет пользователя с таким id' };
+      data.some((user) => {
+        if (user._id === req.params.id) {
+          status = 200;
+          answer = user;
+        }
+      })
+      res.status(status).send(answer);
+    })
+    .catch((err) => res.status(500).send(err))
+  });
+
+module.exports = router;
